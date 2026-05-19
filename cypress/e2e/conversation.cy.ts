09d3763cf429a265cd0da1b486bb606a42a31bea
decade <20120220@student.hcmus.edu.vn>
2026-05-19 19:13:36 +0700

    test: Test implenation for nav logout and group creation

--- cypress/e2e/conversation.cy.ts
+++ cypress/e2e/conversation.cy.ts
@@ -39,4 +39,40 @@ describe('Conversation', () => {
         cy.url().should('include', 'Alice%20Wonderland');
         cy.url().should('include', 'https:%2F%2Fapi.dicebear.com%2F7.x%2Favataaars%2Fsvg%3Fseed%3DAlice');
     });
+    describe('Groups', () => {
+        beforeEach(() => {
+            cy.get('[aria-label="Create Group"]').click();
+            cy.intercept('GET', '**/people**',
+                {fixture: 'get-people-success.json', delay: 2000})
+                .as('people')
+        })
+        it('should be able open group dialog when click create group button', () => {
+            cy.get('Create Group').should('be.visible');
+        });
+        it('should be able to find other people to add to group', () => {
+            cy.get('[placeholder="Search by name..."]').type('Meo meo');
+            cy.contains('Thái Minh Trí').should('be.visible');
+            cy.contains('NAB Colleague').should('be.visible');
+            cy.wait('@people')
+        });
+        it('should should be able to add suggested user as partner', () => {
+            cy.get('[placeholder="Search by name..."]').type('Meo meo');
+            cy.contains('Thái Minh Trí').click()
+            cy.get('.selected-members')
+                .find('Thái Minh Trí')
+                .should('be.visible');
+        });
+        it('should be able to create group when group name is non empty and selected partner >= 1', () => {
+            cy.intercept('POST', '**/groups', {statusCode: 200, body: {}})
+                .as('create-group');
+            cy.get('[placeholder="Enter group name"]').type('Test Group');
+
+            cy.get('[placeholder="Search by name..."]').type('Meo meo');
+            cy.contains('Thái Minh Trí').click()
+            cy.contains('NAB Colleague').click()
+
+            cy.contains(/^Create$/).click()
+            cy.wait('@create-group')
+        });
+    })
 })
